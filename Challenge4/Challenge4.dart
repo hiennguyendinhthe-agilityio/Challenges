de0e9134f49c54d9f31b7927f5eaf4aa973a3c91
hienndt1997@gmail.com <thehien123>
2023-01-05 11:33:18 +0700

    Finished challenge4

---
+++ Challenge4/Challenge4.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  const double  rating1 = 23;
+  const double rating2 = 345.12;
+  const double rating3 = 442.3;
+  var averageRating = (rating1 + rating2 + rating3) / 3;
+
+  print("Calculate the average of the three is $averageRating");
+}
