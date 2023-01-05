c2aca5609fd1fc976dada2d13649a96a1bc79b3e
hienndt1997@gmail.com <thehien123>
2023-01-05 16:37:17 +0700

    Commit add typed Final

--- Challenge5/Challenge5.dart
+++ Challenge5/Challenge5.dart
@@ -15,9 +15,9 @@ void main(List<String> args) {
   const double b = 51;
   const double c = 12;
   const double result = ((b * b) - 4 * a * c );
-  double result1 = sqrt(result);
-  double root1 = (-b + result1) / (2 * a);
-  double root2 = (-b - result1) / (2 * a);
+  final double result1 = sqrt(result);
+  final double root1 = (-b + result1) / (2 * a);
+  final double root2 = (-b - result1) / (2 * a);
 
   print(root1);
   print(root2);
