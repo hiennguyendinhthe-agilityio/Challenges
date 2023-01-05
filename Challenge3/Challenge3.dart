c5c466bca82fcbfc9d7bea765d33858c1e0ae5d2
hienndt1997@gmail.com <thehien123>
2023-01-05 11:17:05 +0700

    finished challenge3

---
+++ Challenge3/Challenge3.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  const x = 46;
+  const y = 10;
+
+  const answer1 = (x * 100) + y;
+  const answer2 = (x * 100) + (y * 100);
+  const answer3 = (x * 100) + (y / 10);
+
+  print("answer1 = $answer1");
+  print("answer2 = $answer2");
+  print("answer3 = $answer3");
+}
